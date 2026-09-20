fbbde743463855f156b936adf98218218c36c1b3
Daniel Moreno <morenodurandanieleduardo1120@gmail.com>
2026-09-19 20:48:29 -0500

    chore: remove compiled Java files

--- apps/api/bin/mvnw.cmd
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-
-SET "MAVEN_PROJECTBASEDIR=%~dp0"
-
-java "-Dmaven.multiModuleProjectDirectory=%MAVEN_PROJECTBASEDIR%" ^
--classpath "%MAVEN_PROJECTBASEDIR%\.mvn\wrapper\maven-wrapper.jar" ^
-org.apache.maven.wrapper.MavenWrapperMain %*
